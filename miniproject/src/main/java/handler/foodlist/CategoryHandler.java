71b7b34e054355d7da0fc39aebbac5ee9f571e94
SkylarLee1003 <skylarlee1003@gmail.com>
2023-04-24 12:35:15 +0900

    FoodList

--- miniproject/src/main/java/handler/foodlist/CategoryHandler.java
+++ miniproject/src/main/java/handler/foodlist/CategoryHandler.java
@@ -5,20 +5,20 @@ import java.util.ArrayList;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import foodmanage.FoodManageService;
+import foodlist.FoodListService;
 import foodmanage.FoodManageVo;
 import handler.Handler;
 
-public class MyListHandler implements Handler {
+public class CategoryHandler implements Handler {
 
 	@Override
 	public String process(HttpServletRequest request, HttpServletResponse response) {
 		// TODO Auto-generated method stub
-		String id = request.getParameter("id");
-		FoodManageService service = new FoodManageService();
-		ArrayList<FoodManageVo> list = service.getById(id);
+		int cat_num=Integer.parseInt( request.getParameter("cat_num"));
+		FoodListService service = new FoodListService();
+		ArrayList<FoodManageVo> list = service.getByCategories(cat_num);
 		request.setAttribute("list", list);
-		request.setAttribute("view", "${pageContext.request.contextPath}/foodlist/mylist.jsp");		
+		request.setAttribute("view", "/foodlist/mylist.jsp");		
 		return null;
 	}
 
